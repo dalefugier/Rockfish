d00123917135171d4eef1a0017b93da1344ce816
Dale Fugier <dale@mcneel.com>
2017-11-01 11:20:59 -0700

    Added additional files

---
+++ RockfishCommon/RockfishGeometry.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using Rhino;
+using Rhino.Geometry;
+
+namespace RockfishCommon
+{
+  [DataContract]
+  public class RockfishGeometry
+  {
+    /// <summary>
+    /// Public constructor
+    /// </summary>
+    /// <param name="src">The RhinoCommon geometry object.</param>
+    public RockfishGeometry(GeometryBase src)
+    {
+      Geometry = src;
+    }
+
+    public GeometryBase Geometry { get; private set; }
+
+    public Curve Curve => Geometry as Curve;
+    public Brep Brep => Geometry as Brep;
+    public Extrusion Extrusion => Geometry as Extrusion;
+    public Mesh Mesh => Geometry as Mesh;
+
+    [DataMember]
+    public byte[] Data
+    {
+      get => ToBytes(Geometry);
+      set => Geometry = ToGeometry(value);
+    }
+
+    /// <summary>
+    /// Converts a object that inherits from Rhino.Geometry.GeometryBase
+    /// to an array of bytes.
+    /// </summary>
+    private static byte[] ToBytes(GeometryBase src)
+    {
+      var rc = new byte[0];
+      if (null == src)
+        return rc;
+
+      try
+      {
+        var formatter = new BinaryFormatter();
+        using (var stream = new MemoryStream())
+        {
+          formatter.Serialize(stream, src);
+          rc = stream.ToArray();
+        }
+      }
+      catch (Exception e)
+      {
+        RhinoApp.WriteLine(e.Message);
+      }
+
+      return rc;
+    }
+
+    public static GeometryBase ToGeometry(byte[] bytes)
+    {
+      if (null == bytes || 0 == bytes.Length)
+        return null;
+
+      GeometryBase rc = null;
+      try
+      {
+        using (var stream = new MemoryStream())
+        {
+          var formatter = new BinaryFormatter();
+          stream.Write(bytes, 0, bytes.Length);
+          stream.Seek(0, SeekOrigin.Begin);
+          var geometry = formatter.Deserialize(stream) as GeometryBase;
+          if (null != geometry && geometry.IsValid)
+            rc = geometry;
+        }
+      }
+      catch (Exception e)
+      {
+        RhinoApp.WriteLine(e.Message);
+      }
+
+      return rc;
+    }
+  }
+}
---
+++ RockfishCommon/RockfishPoint.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.Serialization;
+using Rhino.Geometry;
+
+namespace RockfishCommon
+{
+  [DataContract]
+  public class RockfishPoint
+  {
+    /// <summary>
+    /// Public constructor
+    /// </summary>
+    /// <param name="src">The RhinoCommon Point3d object.</param>
+    public RockfishPoint(Point3d src)
+    {
+      X = src.X;
+      Y = src.Y;
+      Z = src.Z;
+    }
+
+    /// <summary>
+    /// Gets or sets the X (first) coordinate of this point.
+    /// </summary>
+    [DataMember]
+    public double X { get; private set; }
+
+    /// <summary>
+    /// Gets or sets the Y (second) coordinate of this point.
+    /// </summary>
+    [DataMember]
+    public double Y { get; private set; }
+
+    /// <summary>
+    /// Gets or sets the Z (third) coordinate of this point.
+    /// </summary>
+    [DataMember]
+    public double Z { get; private set; }
+
+    /// <summary>
+    /// Get a RhinoCommon Point3d object
+    /// </summary>
+    public Point3d ToPoint3D => new Point3d(X, Y, Z);
+  }
+
+}
